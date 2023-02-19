68feca244e16291c12c84520082c1e6963e66c60
Wenxuan Feng <279357596@qq.com>
2023-02-19 13:12:18 +0800

    feat: add mv

--- main.rb
+++ main.rb
@@ -4,10 +4,20 @@ require './module_scp'
 
 include SCP
 
-host = ARGV[0]
-username = ARGV[1]
-pwd = ARGV[2]
-local_file = ARGV[3]
-remote_file = ARGV[4]
+# SCP Upload
+# host = ARGV[0]
+# username = ARGV[1]
+# pwd = ARGV[2]
+# local_file = ARGV[3]
+# remote_file = ARGV[4]
 
-upload_file(host, username, pwd, local_file, remote_file)
+# upload_file(host, username, pwd, local_file, remote_file)
+
+# Move File
+# ruby main.rb "~/Desktop/prompts" "/Volumes/RAID/dev"
+LOCAL = ARGV[0]
+TARGET = ARGV[1]
+
+zip_file = compress(LOCAL, "dist.zip")
+move(DIR, TARGET)
+unCompressfile(DIR, TARGET)
--- module_scp.rb
+++ module_scp.rb
@@ -2,8 +2,60 @@
 # -*- coding: UTF-8 -*-
 
 require 'net/scp'
+require 'fileutils'
+require 'zip'
 
 module SCP
+    def compress(local_file, file_name)
+        dest_file = File.join(local_file, file_name)
+        
+        puts "#{dest_file} compress #{local_file} ..."
+
+        if !File.exist?(local_file)
+            puts "File does not exist."
+        end
+
+        Zip::File.open(dest_file, Zip::File::CREATE) do |zipfile|
+            puts "entry #{zipfile}"
+            Dir[File.join(local_file), '**', '**'].each do |file|
+                if zipfile.find_entry(file)
+                    puts "File already exists in the zip file: #{zipfile.find_entry(file)}, #{dest_file}" 
+                elsif zipfile.glob(file).any?
+                    puts "File already exists in the zip file."
+                else
+                    zipfile.add(file.sub(local_file + '/', ''), file)
+                end
+            end
+
+            puts "Add all successfully!"
+        
+        end
+
+        return dest_file
+    end
+
+    def move(file_path, target_path)
+        FileUtils.mkdir_p(target_path) unless File.directory?(target_path)
+
+        FileUtils.mv(file_path, target_path)
+        
+        File.delete(file_path)
+    end
+
+    def unCompressfile(zip_file, target_dir)
+        Zip::file.open(zip_file) do |zipfile|
+            zipfile.each do |file|
+                fpath = File.join(target_dir, file.name)
+                FileUtils.mkdir_p(File.dirname(fpath))
+                zipfile.extract(file, fpath) unless File.exist?(fpath)
+            end
+        end
+
+        puts "Unzip successfully."
+        File.delete(zip_file)
+        puts "Deleted #{zip_file} on remote machine."
+    end
+
     def upload_file(host, username, pwd, local_file, remote_file)
         Net::SCP.start(host, username, :password => pwd) do |scp|
             scp.upload(local_file, remote_file)
